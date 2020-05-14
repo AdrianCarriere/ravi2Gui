bcd0b5d86f690753a7f9d59d757055a2423d5125
AdrianCarriere <carriere.adrian@gmail.com>
2020-05-14 15:12:13 +0200

    changement taille

--- gui.py
+++ gui.py
@@ -12,7 +12,7 @@ class Gui(QMainWindow):
     def initUI(self):
         self.statusBar().showMessage('Ready')
 
-        self.setGeometry(300, 300, 250, 150)
+        self.setGeometry(1280, 720, 250, 150)
         self.setWindowTitle('Fenêtre')
         self.show()
 
